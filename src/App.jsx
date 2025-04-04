b4de64e97564d47f446c24c63c1965f8558313fc
Nodirbek <shamsiddinovnodirbek469@gmail.com>
2025-04-04 19:50:21 +0500

    ddddddddddddd

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ const App = () => {
   return (
     <div>
       <h1>Lorem ipsum dolor sit.</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, tempore!</p>
+      <p>Lorem ipsum dolor, sit  amet consectetur adipisicing elit. Repellendus, tempore!</p>
     </div>
   );
 };
 
 export default App;
+ 
